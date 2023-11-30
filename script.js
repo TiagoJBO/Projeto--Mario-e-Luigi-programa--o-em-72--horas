14263fc0c1a82933dfba2dc2d1aeaf2ef5edf668
tiagoJBO <timarao2@hotmail.com>
2023-11-29 22:41:31 -0300

    Projeto Mario e Luigi

---
+++ script.js
@@ -0,0 +1,15 @@
+let formulario = document.querySelector(".fala-conosco")
+let mascara = document.querySelector(".mascara-form")
+
+function cliqueiNoBotao() {
+
+    formulario.style.left = "800px"
+    mascara.style.visibility = ("visible")
+
+}
+
+function sumirFormulario() {
+
+    formulario.style.left = "-330px"
+    mascara.style.visibility = ("hidden")
+}
