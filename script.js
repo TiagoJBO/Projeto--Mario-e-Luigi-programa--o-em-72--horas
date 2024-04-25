3cc3a6ef27a86bc0d35daef1dea5953a39781058
TiagoJBO <timarao2@hotmail.com>
2024-04-24 23:27:07 -0300

    Melhorando e atualizando

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let formulario = document.querySelector(".fala-conosco")
-let mascara = document.querySelector(".mascara-form")
+let formulario = document.querySelector(".fala-conosco");
+let mascara = document.querySelector(".mascara-form");
 
 function cliqueiNoBotao() {
-
-    formulario.style.left = "800px"
-    mascara.style.visibility = ("visible")
-
+  formulario.style.left = "50%";
+  formulario.style.transform = "translatex(-50%)";
+  mascara.style.visibility = "visible";
 }
 
 function sumirFormulario() {
-
-    formulario.style.left = "-330px"
-    mascara.style.visibility = ("hidden")
+  formulario.style.left = "-330px";
+  mascara.style.visibility = "hidden";
+}
+function clickMasc() {
+  formulario.style.left = "-330px";
+  mascara.style.visibility = "hidden";
 }
